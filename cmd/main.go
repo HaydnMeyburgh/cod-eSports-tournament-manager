0ea2faa824698919c2ac22f4d0492f1be9c61615
Haydn <haydnmey@gmail.com>
2023-08-31 13:25:36 +0200

    setup the initial server

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,17 @@
 package main
 
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
 func main() {
-	
+	router := gin.Default()
+	router.GET("/", func(ctx *gin.Context) {
+		ctx.IndentedJSON(http.StatusOK, gin.H{
+			"message": "Hello there, server is running",
+		})
+	})
+	router.Run() // Listening and serving on 0.0.0.0:8080
 }

f7a148fa0decb472fb0fccc2fbdb483f70201e7f
Haydn <haydnmey@gmail.com>
2023-09-01 11:42:07 +0200

    added error handling for server and routes

--- cmd/main.go
+++ cmd/main.go
@@ -1,17 +1,35 @@
 package main
 
 import (
+	"log"
 	"net/http"
+	"os"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080" // Default port
+	}
+
 	router := gin.Default()
 	router.GET("/", func(ctx *gin.Context) {
 		ctx.IndentedJSON(http.StatusOK, gin.H{
 			"message": "Hello there, server is running",
 		})
 	})
-	router.Run() // Listening and serving on 0.0.0.0:8080
+
+	// error handling for routes that aren't defined
+	router.NoRoute(func(ctx *gin.Context) {
+		ctx.IndentedJSON(http.StatusNotFound, gin.H{
+			"error": "Route not found",
+		})
+	})
+
+	// Log error if server can't start
+	if err := router.Run(":" + port); err != nil {
+		log.Fatalf("Error starting server: %v", err)
+	}
 }

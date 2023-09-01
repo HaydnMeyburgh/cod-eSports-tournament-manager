c6e2a6265a0f3c7ca9c74fd147c05887ce933349
Haydn <haydnmey@gmail.com>
2023-09-01 12:50:06 +0200

    connect to mongodb and load env variables

--- cmd/main.go
+++ cmd/main.go
@@ -1,20 +1,39 @@
+// Exectuable package
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
 	"os"
 
 	"github.com/gin-gonic/gin"
+	"github.com/haydnmeyburgh/cod-eSports-tournament-manager/internal/database"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	// Load environment variables from the .env file
+	if err := godotenv.Load(); err != nil {
+		log.Fatalf("Error loading .env file: %v", err)
+	}
+
+	// Connect to MongoDB
+	if err := database.ConnectToMongoDB(); err != nil {
+		log.Fatalf("Failed to connect to MongoDB: %v", err)
+	}
+	defer database.GetMongoClient().Disconnect(context.TODO())
+
+	// Get server port from env variable or use default
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8080" // Default port
 	}
 
+	// Gin router creation
 	router := gin.Default()
+
+	// SImple root route
 	router.GET("/", func(ctx *gin.Context) {
 		ctx.IndentedJSON(http.StatusOK, gin.H{
 			"message": "Hello there, server is running",
@@ -28,7 +47,7 @@ func main() {
 		})
 	})
 
-	// Log error if server can't start
+	// Start server, or log error if problem with server starting
 	if err := router.Run(":" + port); err != nil {
 		log.Fatalf("Error starting server: %v", err)
 	}

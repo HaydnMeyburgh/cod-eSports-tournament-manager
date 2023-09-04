c16fe30f5934110214e4495b285ff58cd3865f09
Haydn <haydnmey@gmail.com>
2023-09-04 11:03:48 +0200

    Created UpdateUser handler

--- internal/handlers/user.go
+++ internal/handlers/user.go
@@ -47,14 +47,31 @@ func (h *UserHandler) LoginUser(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{"token": token})
 }
 
-// Handles user logout
+// Handles user logout and clears cookie
 func (h *UserHandler) LogoutUser(c *gin.Context) {
-
+	models.LogoutUser(c)
 }
 
 // Handles user profile updates
 func (h *UserHandler) UpdateUser(c *gin.Context) {
+	var updateUser struct {
+		Username string `json:"username"`
+		Password string `json:"password"`
+	}
 
+	if err := c.ShouldBindJSON(&updateUser); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	userID := c.MustGet("user_id").(string)
+	err := models.UpdateUser(c, userID)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to update user"})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"message": "User updated successfully"})
 }
 
 func NewUserHandler() *UserHandler {

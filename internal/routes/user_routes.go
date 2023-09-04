0ccd152abd70f990ebcd01ddc7d2d7fc82ba04a6
Haydn <haydnmey@gmail.com>
2023-09-04 11:52:37 +0200

    added authMiddleware for routes needing authentication

--- internal/routes/user_routes.go
+++ internal/routes/user_routes.go
@@ -1,16 +1,28 @@
 package routes
 
 import (
+	"log"
+	"os"
+
 	"github.com/gin-gonic/gin"
+	"github.com/haydnmeyburgh/cod-eSports-tournament-manager/internal/auth"
 	"github.com/haydnmeyburgh/cod-eSports-tournament-manager/internal/handlers"
 )
 
 // Setup user routes
 func SetupUserRoutes(r *gin.Engine, userHandler *handlers.UserHandler) {
+	jwtSecret := os.Getenv("SECRET_KEY")
+	if jwtSecret == "" {
+		log.Fatalf("SECRET_KEY environment variable is not set")
+	}
+
 	userRoutes := r.Group("/users")
 	{
 		userRoutes.POST("/register", userHandler.RegisterUser)
 		userRoutes.POST("/login", userHandler.LoginUser)
+		
+		userRoutes.Use(auth.AuthMiddleware(jwtSecret))
+		
 		userRoutes.POST("/logout", userHandler.LogoutUser)
 		userRoutes.PUT("/update", userHandler.UpdateUser)
 	}

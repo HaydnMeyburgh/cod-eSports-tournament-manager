2d9fd991380580566016dcfcb632c4044fe0218c
Haydn <haydnmey@gmail.com>
2023-09-04 10:50:47 +0200

    Create LoginUser handler

--- internal/handlers/user.go
+++ internal/handlers/user.go
@@ -2,6 +2,7 @@ package handlers
 
 import (
 	"net/http"
+
 	"github.com/gin-gonic/gin"
 	"github.com/haydnmeyburgh/cod-eSports-tournament-manager/internal/models"
 )
@@ -9,9 +10,9 @@ import (
 type UserHandler struct{}
 
 // Handles user registration
-func(h *UserHandler) RegisterUser(c *gin.Context) {
+func (h *UserHandler) RegisterUser(c *gin.Context) {
 	var newUser models.User
-	
+
 	if err := c.ShouldBindJSON(&newUser); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
@@ -26,18 +27,34 @@ func(h *UserHandler) RegisterUser(c *gin.Context) {
 }
 
 // Handles user login
-func(h *UserHandler) LoginUser(c *gin.Context) {
-	
+func (h *UserHandler) LoginUser(c *gin.Context) {
+	var loginUser struct {
+		Email    string `json:"email" binding:"required"`
+		Password string `json:"Password" binding:"required"`
+	}
+
+	if err := c.ShouldBindJSON(&loginUser); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	token, err := models.LoginUser(c)
+	if err != nil {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid credentials"})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"token": token})
 }
 
 // Handles user logout
-func(h *UserHandler) LogoutUser(c *gin.Context) {
-	
+func (h *UserHandler) LogoutUser(c *gin.Context) {
+
 }
 
 // Handles user profile updates
-func(h *UserHandler) UpdateUser(c *gin.Context) {
-	
+func (h *UserHandler) UpdateUser(c *gin.Context) {
+
 }
 
 func NewUserHandler() *UserHandler {

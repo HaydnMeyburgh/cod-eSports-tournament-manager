2d80f76fb5baa41f126a1314bc648be43b55eb57
Haydn <haydnmey@gmail.com>
2023-09-04 11:18:07 +0200

    created user routes

---
+++ internal/routes/user_routes.go
@@ -0,0 +1,17 @@
+package routes
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/haydnmeyburgh/cod-eSports-tournament-manager/internal/handlers"
+)
+
+// Setup user routes
+func SetupUserRoutes(r *gin.Engine, userHandler *handlers.UserHandler) {
+	userRoutes := r.Group("/users")
+	{
+		userRoutes.POST("/register", userHandler.RegisterUser)
+		userRoutes.POST("/login", userHandler.LoginUser)
+		userRoutes.POST("/logout", userHandler.LogoutUser)
+		userRoutes.POST("/update", userHandler.UpdateUser)
+	}
+}

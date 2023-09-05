d538fbc68638b05a6372a8ac5aa991b831e58277
Haydn <haydnmey@gmail.com>
2023-09-05 12:13:21 +0200

    Create GetTeamByID handler

--- internal/handlers/team_handler.go
+++ internal/handlers/team_handler.go
@@ -10,6 +10,7 @@ import (
 
 type TeamHandler struct{}
 
+// Handler for create team
 func (h *TeamHandler) CreateTeam(c *gin.Context) {
 	var newTeam models.Team
 
@@ -26,4 +27,24 @@ func (h *TeamHandler) CreateTeam(c *gin.Context) {
 
 	c.JSON(http.StatusCreated, createdTeam)
 }
+
+// Handler to for getting a team by ID
+func (h *TeamHandler) GetTeamByID(c *gin.Context) {
+	teamID := c.Param("id")
+
+	objectID, err := primitive.ObjectIDFromHex(teamID)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid team ID format"})
+		return
+	}
+
+	team, err := models.GetTeamByID(c, objectID)
+	if err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, team)
+}
+
 

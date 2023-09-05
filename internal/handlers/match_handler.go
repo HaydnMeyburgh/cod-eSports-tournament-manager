3cb7e09e2d025a9b65f196fb274e4bbf42496d88
Haydn <haydnmey@gmail.com>
2023-09-05 21:57:20 +0200

    handler for updating existing match

--- internal/handlers/match_handler.go
+++ internal/handlers/match_handler.go
@@ -11,7 +11,7 @@ import (
 type MatchHandler struct {}
 
 // Handlers creation of a new match
-func (h *MatchHandler) CreateMatchHandler(c *gin.Context) {
+func (h *MatchHandler) CreateMatch(c *gin.Context) {
 	var newMatch models.Match
 
 	if err := c.ShouldBindJSON(&newMatch); err != nil {
@@ -37,7 +37,7 @@ func (h *MatchHandler) CreateMatchHandler(c *gin.Context) {
 }
 
 // handles the retrieval of a match
-func (h *MatchHandler) GetMatchHandler(c *gin.Context) {
+func (h *MatchHandler) GetMatchByID(c *gin.Context) {
 	matchID := c.Param("id")
 
 	id, err := primitive.ObjectIDFromHex(matchID)
@@ -53,4 +53,30 @@ func (h *MatchHandler) GetMatchHandler(c *gin.Context) {
 	}
 
 	c.JSON(http.StatusOK, match)
+}
+
+// Handles the updating of an existing match.
+func (h *MatchHandler) UpdateMatch(c *gin.Context) {
+	matchID := c.Param("id")
+
+	id, err := primitive.ObjectIDFromHex(matchID)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid Match ID format"})
+		return
+	}
+
+	var updatedMatch models.Match
+
+	if err := c.ShouldBindJSON(&updatedMatch); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	err = models.UpdateMatch(c, id, &updatedMatch)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"message": "Match updated successfully"})
 }

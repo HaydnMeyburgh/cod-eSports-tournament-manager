ad3ec0c8eb66e5a5d257d8d0b0b15fca132c54eb
Haydn <haydnmey@gmail.com>
2023-09-05 23:32:13 +0200

    setup match results routes

---
+++ internal/routes/match_result_routes.go
@@ -0,0 +1,29 @@
+package routes
+
+import (
+	"log"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	"github.com/haydnmeyburgh/cod-eSports-tournament-manager/internal/handlers"
+	"github.com/haydnmeyburgh/cod-eSports-tournament-manager/internal/auth"
+)
+
+// Setup user routes
+func SetupMatchResultRoutes(r *gin.Engine, matchResultHandler *handlers.MatchResultHandler) {
+	jwtSecret := os.Getenv("SECRET_KEY")
+	if jwtSecret == "" {
+		log.Fatalf("SECRET_KEY environment variable is not set")
+	}
+
+	matchResultRoutes := r.Group("/match-results")
+	{
+		matchResultRoutes.GET("/:id", matchResultHandler.GetMatchResultById)
+
+		matchResultRoutes.Use(auth.AuthMiddleware(jwtSecret))
+
+		matchResultRoutes.POST("/", matchResultHandler.CreateMatchResult)
+		matchResultRoutes.PUT("/:id", matchResultHandler.UpdateMatchResult)
+		matchResultRoutes.DELETE("/:id", matchResultHandler.DeleteMatchResult)
+	}
+}

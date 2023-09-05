dc48789cfb8e18bad70270b79155b0e61ce7308f
Haydn <haydnmey@gmail.com>
2023-09-05 22:35:58 +0200

    added auth middleware route to be used

--- internal/routes/team_routes.go
+++ internal/routes/team_routes.go
@@ -1,16 +1,28 @@
 package routes
 
 import (
+	"log"
+	"os"
+
 	"github.com/gin-gonic/gin"
 	"github.com/haydnmeyburgh/cod-eSports-tournament-manager/internal/handlers"
+	"github.com/haydnmeyburgh/cod-eSports-tournament-manager/internal/auth"
 )
 
 // Setup user routes
 func SetupTeamRoutes(r *gin.Engine, teamHandler *handlers.TeamHandler) {
+	jwtSecret := os.Getenv("SECRET_KEY")
+	if jwtSecret == "" {
+		log.Fatalf("SECRET_KEY environment variable is not set")
+	}
+
 	teamRoutes := r.Group("/teams")
 	{
-		teamRoutes.POST("/", teamHandler.CreateTeam)
 		teamRoutes.GET("/:id", teamHandler.GetTeamByID)
+
+		teamRoutes.Use(auth.AuthMiddleware(jwtSecret))
+
+		teamRoutes.POST("/", teamHandler.CreateTeam)
 		teamRoutes.PUT("/:id", teamHandler.UpdateTeam)
 		teamRoutes.DELETE("/:id", teamHandler.DeleteTeam)
 	}

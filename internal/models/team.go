c176431c830565f68879c7cd51041c04472e1024
Haydn <haydnmey@gmail.com>
2023-09-05 11:44:37 +0200

    Function to create team

--- internal/models/team.go
+++ internal/models/team.go
@@ -1,1 +1,31 @@
 package models
+
+import (
+	"context"
+	"errors"
+
+	"github.com/gin-gonic/gin"
+	"github.com/haydnmeyburgh/cod-eSports-tournament-manager/database"
+	"github.com/haydnmeyburgh/cod-eSports-tournament-manager/internal/database"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-drivers/bson"
+	"go.mongodb.org/mongo-drivers/bson/primitive"
+)
+
+type Team struct {
+	ID primitive.ObjectID `bson:"_id,omitempty"`
+	Name string `bson:"name" binding:"required"`
+	Players []string `bson:"players"`
+}
+
+func CreateTeam(c *gin.Context, team *Team) (*Team, error) {
+	collection := database.GetMongoClient().Database("esports-tournament-manager").Collection("teams")
+
+	result, err := collection.InsertOne(c, team)
+	if err != nil {
+		return nil, err
+	}
+
+	team.ID = result.InsertedID.(primitive.ObjectID)
+	return team, nil
+}

c8b85cebf7491b4392e9dc60c9feb64c01f98bb3
Haydn <haydnmey@gmail.com>
2023-09-05 23:26:54 +0200

    handler to delete a match result

--- internal/handlers/match_result_handler.go
+++ internal/handlers/match_result_handler.go
@@ -41,7 +41,7 @@ func (h *MatchResultHandler) CreateMatchResult(c *gin.Context) {
 	c.JSON(http.StatusCreated, createdMatchResult)
 }
 
-// GetMatchResultHandler handles the retrieval of a match result by its ID.
+// Handles the retrieval of a match result by its ID.
 func (h *MatchResultHandler) GetMatchResultById(c *gin.Context) {
 	matchResultID := c.Param("id")
 
@@ -60,7 +60,7 @@ func (h *MatchResultHandler) GetMatchResultById(c *gin.Context) {
 	c.JSON(http.StatusOK, matchResult)
 }
 
-// UpdateMatchResultHandler handles the updating of an existing match result.
+// Handles the updating of an existing match result.
 func (h *MatchResultHandler) UpdateMatchResult(c *gin.Context) {
 	matchResultID := c.Param("id")
 
@@ -101,4 +101,45 @@ func (h *MatchResultHandler) UpdateMatchResult(c *gin.Context) {
 	}
 
 	c.JSON(http.StatusOK, gin.H{"message": "Match Result updated successfully"})
+}
+
+// Handles the deletion of a match result by its ID.
+func (h *MatchResultHandler) DeleteMatchResult(c *gin.Context) {
+	matchResultID := c.Param("id")
+
+	id, err := primitive.ObjectIDFromHex(matchResultID)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid Match Result ID format"})
+		return
+	}
+
+	userIDStr, err := models.GetUserIDFromContext(c)
+	if err != nil {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "Unauthorized"})
+		return
+	}
+	userID, err := primitive.ObjectIDFromHex(userIDStr)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid user ID format"})
+		return
+	}
+
+	matchResult, err := models.GetMatchResultByID(c, id)
+	if err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": err.Error()})
+		return
+	}
+
+	if userID != matchResult.OrganizerID {
+		c.JSON(http.StatusForbidden, gin.H{"error": "You are not the organizer of this tournament"})
+		return
+	}
+
+	err = models.DeleteMatchResult(c, id)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"message": "Match Result deleted successfully"})
 }

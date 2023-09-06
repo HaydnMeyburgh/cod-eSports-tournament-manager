b31a21cb2e79afd509e2c57b1a88fa4d4f2d5928
Haydn <haydnmey@gmail.com>
2023-09-06 14:44:03 +0200

    websocket messages for create and update functions

--- internal/models/team.go
+++ internal/models/team.go
@@ -1,21 +1,25 @@
 package models
 
 import (
+	"encoding/json"
 	"errors"
+	"log"
 
 	"github.com/gin-gonic/gin"
 	"github.com/haydnmeyburgh/cod-eSports-tournament-manager/internal/database"
+	"github.com/haydnmeyburgh/cod-eSports-tournament-manager/internal/realtimemanager"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
 type Team struct {
-	ID      primitive.ObjectID `bson:"_id,omitempty"`
-	Name    string             `bson:"name" binding:"required"`
-	OrganiserID primitive.ObjectID   `bson:"organiser_id" binding:"required"`
-	Players []string           `bson:"players"`
+	ID           primitive.ObjectID `bson:"_id,omitempty"`
+	Name         string             `bson:"name" binding:"required"`
+	OrganiserID  primitive.ObjectID `bson:"organiser_id" binding:"required"`
+	Players      []string           `bson:"players"`
 	TournamentID primitive.ObjectID `bson:"tournament_id,omitempty"`
+	WebSocketHub *realtimemanager.WebSocketHub
 }
 
 // add a team to a tournament
@@ -42,6 +46,25 @@ func CreateTeam(c *gin.Context, team *Team) (*Team, error) {
 	}
 
 	team.ID = result.InsertedID.(primitive.ObjectID)
+
+	// Broadcast the message to WebSocket clients
+	message := map[string]interface{}{
+		"action":        "team_created",
+		"match_id":      team.ID.Hex(),
+		"name":          team.Name,
+		"players":       team.Players,
+		"tournament_id": team.TournamentID.Hex(),
+	}
+
+	// Marshal the message to JSON
+	messageJSON, err := json.Marshal(message)
+	if err != nil {
+		log.Printf("Error marshaling WebSocket message: %v", err)
+		return team, nil
+	}
+
+	team.WebSocketHub.Broadcast(messageJSON)
+
 	return team, nil
 }
 
@@ -96,6 +119,23 @@ func UpdateTeam(c *gin.Context, id primitive.ObjectID, updatedTeam *Team) error 
 	if err != nil {
 		return err
 	}
+
+	// Broadcast the message to WebSocket clients
+	message := map[string]interface{}{
+		"action":        "team_created",
+		"match_id":      updatedTeam.ID.Hex(),
+		"name":          updatedTeam.Name,
+		"players":       updatedTeam.Players,
+		"tournament_id": updatedTeam.TournamentID.Hex(),
+	}
+
+	// Marshal the message to JSON
+	messageJSON, err := json.Marshal(message)
+	if err != nil {
+		return err
+	}
+
+	updatedTeam.WebSocketHub.Broadcast(messageJSON)
 
 	return nil
 }
